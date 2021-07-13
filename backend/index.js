a77d7d6cada322164bc3c97cf24d0dbf1fdf1e96
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-07-13 20:30:31 +0300

    added routes to index.js

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
 
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
-        useUnifiedTopology: true 
+        useUnifiedTopology: true,
+        useCreateIndex: true,
     })
     .then(()  => {
         console.log('Mongo DB Connected');
     })
     .catch((err) => console.log(err));
 
-
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
 
 app.listen(8800, () => {
     console.log("Server is running....")
